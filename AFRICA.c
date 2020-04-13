089899bf79a0b9578376edf41eb31a0b7ca2daf4
dxtdbdd02 <63539110+dxtdbdd02@users.noreply.github.com>
2020-04-13 15:10:59 +0800

    Add files via upload

---
+++ AFRICA.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define length 5
+#define width 3
+/*
+double area()
+{
+     const double AGE = 5;
+     const char NAME[] = "America";
+     double area = length * width;
+     printf("%f\t%s\n", AGE, NAME);
+     int v1 = 1;
+     float v2 = 1.5;
+     char v3 = 'a';
+     double v4 = 3.567;
+     printf("sizeof(v1) = %d\tsizeof(v2) = %d\tsizeof(v3) = %d\tsizeof(v4) = %d\n", sizeof(v1), sizeof(v2), sizeof(v3), sizeof(v4));
+     //AGE = 50
+     int array1[5] = {1, 2, 3, 4, 5};
+     printf("sizeof(array1) = %d\n", sizeof(array1));
+     return area;
+}
+int main()
+{
+    double area1 = area();
+    printf("%f\n", area1);
+    return 0;
+}
+*/
